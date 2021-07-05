d897ec293d83ebc6e1c154a47d11880082d6d35f
DalitsoKasonde <dkasonde11@gmail.com>
2021-07-06 00:34:36 +0200

    change parts of site

--- home.js
+++ home.js
@@ -1,16 +1,1 @@
-let key = 42;
-
-function getSecretCode(value) {
-  let keyGenerator = function () {
-    let key = 12;
-    console.log("in keyGenerator: ", key);
-    return key;
-  };
-
-  let code = value * keyGenerator();
-  console.log("in getSecretCode: ", key);
-  return code;
-}
-
-let secretCode = getSecretCode(2);
-showMessage(secretCode);
+changePercentOff(32);
