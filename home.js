1ca41489b36321851d687a911f9e603f1d61b76e
DalitsoKasonde <dkasonde11@gmail.com>
2021-07-07 03:43:40 +0200

    object methods

--- home.js
+++ home.js
@@ -1,1 +1,10 @@
-changePercentOff(32);
+let person = {
+  name: "John",
+  age: 32,
+  partTime: false,
+  showInfo: function (realAge) {
+    showMessage(this.name + " is " + realAge);
+  },
+};
+
+showMessage(typeof person.showInfo);
